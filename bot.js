dcf009a972f3030c2f9cb8a0386c9411c092d3b5
metix3255 <mateusz147@vp.pl>
2019-08-24 22:03:43 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,6 +6,20 @@
 
 const DBM = {};
 const DiscordJS = DBM.DiscordJS = require('discord.js');
+const express = require('express');
+const keepalive = require('express-glitch-keepalive');
+
+const app = express();
+
+app.use(keepalive);
+
+app.get('/', (req, res) => {
+res.json('This bot should be online! Uptimerobot will keep it alive');
+});
+app.get("/", (request, response) => {
+response.sendStatus(200);
+});
+app.listen(process.env.PORT);
 
 //---------------------------------------------------------------------
 // Bot
